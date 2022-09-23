1aa7a9c2b2c1c1e98141f812687497158f3bc35c
marika <marika@waifu.club>
2022-09-22 21:50:22 -0300

    Add the template SwiftUI project.

---
+++ SpiceCompanion/SpiceCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiceCompanionApp.swift
+//  SpiceCompanion
+//
+//  Created by marika on 2022-09-22.
+//
+
+import SwiftUI
+
+@main
+struct SpiceCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
