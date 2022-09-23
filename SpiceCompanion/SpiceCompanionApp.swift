9fc11dc2de34e89a414df77d859b74e47f53238e
marika <marika@waifu.club>
2022-09-22 22:06:10 -0300

    Add an empty server list.

--- SpiceCompanion/SpiceCompanionApp.swift
+++ SpiceCompanion/SpiceCompanionApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SpiceCompanionApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ServerList()
+            }
+            .navigationViewStyle(.stack)
         }
     }
 }
